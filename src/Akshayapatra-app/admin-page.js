e0280660af1c3299a6b9093594e5dc79757ac043
amitkumar <amitkumar392@gmail.com>
2020-02-14 18:43:12 +0530

    commit 13

--- src/Akshayapatra-app/admin-page.js
+++ src/Akshayapatra-app/admin-page.js
@@ -68,15 +68,17 @@ class AdminPage extends PolymerElement {
 <p>Server error</p>
 </div>
 <app-location route={{route}}></app-location>
-      <!--<highcharts-chart type="spline" data='[[0,0],[1,7],[2,1],[3,6],[4,8],[5,6]]' title='Test-Spline Chart' x-zoom x-label="Iterations" y-label="Awesomeness Index"></highcharts-chart>-->
-      <highcharts-chart id="myChart" type = 'pie' data = {{data}} title="Chart For All The Donations Schemes" export = "true" on-click="_handleClick"></highcharts-chart>
-      <iron-ajax id="ajax" handle-as="json" on-response="_handleResponse" 
-      content-type="application/json" on-error="_handleError"></iron-ajax>
+
+<iron-ajax id="ajax" handle-as="json" on-response="_handleResponse" content-type="application/json" on-error="_handleError"></iron-ajax>
+    
+
+<highcharts-chart id="myChart" colorByPoint='true' type = 'pie' data = {{data}} title="Chart For All The Donations Schemes" export = "true" on-click="_handleClick"></highcharts-chart>
+      
       <paper-dialog id="dialog" entry-animation="scale-up-animation"
       exit-animation="fade-out-animation" >
          <iron-icon icon="clear" id="clearbtn" on-click="_handleClose"></iron-icon>
          <table id="tab1">
-           <h2>Lists of Donars</h2>
+           <h2>Lists of Donors</h2>
          <tr>
 
              <th>Scheme Name</th>
@@ -141,13 +143,14 @@ class AdminPage extends PolymerElement {
   // as soon as page load make ajax call method will run
   connectedCallback(){
     super.connectedCallback();
-    this._makeAjax('http://localhost:3000/schemes','get',null)
+    this._makeAjax('http://10.117.189.37:9090/akshayapathra/schemes/analysis','get',null)
   }
   // reading properties values from event
   _handleClick(event) {
     let { name, y, schemeId} = event.point.options;
     console.log(name, y, schemeId)
     this.$.dialog.open();
+    
     this._makeAjax(`http://10.117.189.37:9090/akshayapathra/schemes/${schemeId}`,'get',null);
     this.action = 'DonarDetail'
   }
